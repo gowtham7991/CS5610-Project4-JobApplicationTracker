ccd06eddd1d19af675522c55360521e8c55e85a9
renuk21 <renukaganesh21@gmail.com>
2022-12-06 00:26:57 -0500

    student profile

--- src/app/student/profile/view_mode/contact_info.js
+++
@@ -1,4 +0,0 @@
-const ProfileContact = () => {
-
-}
-export default ProfileContact;
--- src/app/student/profile/view_mode/profile_basic.js
+++ src/app/student/profile/view_mode/profile_basic.js
@@ -7,37 +7,60 @@ const ProfileComponent = () => {
     const profile = useSelector(state => state.profile)
     return (
         profile.map(profile =>
-            <div className="container position-relative">
+            <div className="container position-relative bg-light pb-2 border">
+                <div className="container bg-white border rounded-2">
                 <div className="row">
                     <div className="col-12">
-                        <img src={"../images/" + profile.bannerPicture} width="100%" height="250px" alt=""/>
+                        <img src={profile.bannerPicture} width="100%" height="250px" alt=""/>
                         <div className="wd-profile-basic">
-                            <img src={"../images/" + profile.profilePicture} height="150px" width="150px"
+                            <img src={profile.profilePicture} height="150px" width="150px"
                                  className="rounded-circle wd-profile-picture bg-white ms-2" alt=""/>
                         </div>
                     </div>
                 </div>
-                <div className="container">
+
                     <div className="wd-profile-basic">
                         <span><h2 className="m-0">{profile.firstName} {profile.lastName}</h2>
                         <Link to="/student/edit-profile">
                             <button className="btn btn-outline-primary rounded-pill float-end m-2">Edit Profile
                             </button>
                         </Link></span>
-                        <div className="mb-3">{profile.summary}</div>
-                        <div className="mb-3">{profile.about}</div>
-                        <div className="mb-3">Website: {profile.website}</div>
-                        <div className="row mb-3">
-                            <div className="col-3">{profile.location}</div>
-                            <div className="col-3">
-                                <Link to="/student/contact">
-                                    <b className="text-primary">contact info</b>
-                                </Link>
-                            </div>
+                        <div className="mb-3">{profile.headline}</div>
+                        <div className="mb-3"><b>Website:</b> <a href={profile.website}>{profile.website}</a></div>
+                        <div className="mb-3"><b>Location:</b> {profile.location}</div>
+                        <div className="mb-3"><b>Contact info:</b>
+                            <a href={profile.email}> {profile.email}</a>
                         </div>
+                        <div className="mb-3">{profile.summary}</div>
                     </div>
                 </div>
-            </div>)
+                <div className="container bg-white border rounded-2 mt-4">
+                    <div className="mb-2 mt-2"><h4>About</h4></div>
+                    {profile.about}
+                </div>
+
+                <div className="container bg-white border rounded-2 mt-4">
+                    <div className="mb-2 mt-2"><h4>Education</h4></div>
+                    {profile.course}
+                    {profile.about_course}
+                </div>
+
+                <div className="container bg-white border rounded-2 mt-4">
+                    <div className="mb-2 mt-2"><h4>Courses</h4></div>
+                    {profile.course}
+                    {profile.about_course}
+                </div>
+                <div className="container bg-white border rounded-2 mt-4">
+                    <div className="mb-2 mt-2"><h4>Work Experience</h4></div>
+                    {profile.course}
+                    {profile.about_course}
+                </div>
+                <div className="container bg-white border rounded-2 mt-4">
+                    <div className="mb-2 mt-2"><h4>Skills</h4></div>
+                    {profile.about}
+                </div>
+            </div>
+    )
     )
 }
 
