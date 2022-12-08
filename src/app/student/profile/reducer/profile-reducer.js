c4ca7f810555621712904ffe254f5193096acc69
renuk21 <renukaganesh21@gmail.com>
2022-12-07 23:50:06 -0500

    student profile

--- src/app/student/profile/reducer/profile-reducer.js
+++ src/app/student/profile/reducer/profile-reducer.js
@@ -1,47 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
-import profile from "../data/profile.json";
+import user from "../data/profile.json";
 import { findProfileThunk } from "../../../../services/profile/profile-thunks"
 
-const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-const initialState = {
-    profile: profile,
-    loading: false
-}
+// const initialState = {
+//     user: user,
+//     loading: false
+// }
 const profileSlice = createSlice({
-    name: "profile",
-    initialState,
-    extraReducers: {
+    name: "user",
+    initialState: user,
+    reducers: {
         [findProfileThunk.pending]:
             (state) => {
                 state.loading = true
-                state.profile = []
+                state.user = []
             },
         [findProfileThunk.fulfilled]:
             (state, {payload}) => {
                 state.loading = false
-                state.profile = payload
+                state.user = payload
             },
         [findProfileThunk.rejected]:
             (state) => {
                 state.loading = false
             }
-    },
-    reducers: {
-        editProfile(state, action) {
-            state[0].firstName = action.payload.profile["name"].split(" ")[0]
-            state[0].lastName = action.payload.profile["name"].split(" ")[1]
-            state[0].bio = action.payload.profile["bio"]
-            state[0].location = action.payload.profile["location"]
-            state[0].website = action.payload.profile["website"]
-            let dateOfBirthFormat;
-            dateOfBirthFormat = action.payload.profile["birthdate"].split("-");
-            let newDOB;
-            newDOB = dateOfBirthFormat[2] + " " + monthNames[dateOfBirthFormat[1] - 1] + " " + dateOfBirthFormat[0];
-            state[0].dateOfBirth = newDOB
-        }
     }
 });
 
-export const {editProfile} = profileSlice.actions
 export default profileSlice.reducer;
--- src/app/student/profile/view_mode/index.js
+++ src/app/student/profile/view_mode/index.js
@@ -1,82 +1,83 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import '../index.css';
 import { findProfileThunk } from "../../../../services/profile/profile-thunks"
 
-const ProfileComponent = ({email}) => {
-    const profile = useSelector(state => state.profile)
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(findProfileThunk({email}))
-    }, []);
-    return (
-        profile.map(profile =>
+const ProfileComponent = () => {
+// const ProfileComponent = ({email}) => {
+    const user = useSelector(state => state.user)
+    // const dispatch = useDispatch();
+    // useEffect(() => {
+    //     dispatch(findProfileThunk({email}))
+    // }, []);
+    return (user.map(user =>
             <div className="container position-relative bg-light pb-2 border">
                 <div className="container bg-white border rounded-2">
                     <div className="row">
                         <div className="col-12">
-                            <img src={profile.bannerPicture} width="100%" height="250px" alt=""/>
+                            <img src={user.profile.banner} width="100%" height="250px" alt=""/>
                             <div className="wd-profile-basic">
-                                <img src={profile.image} height="150px" width="150px"
+                                <img src={user.profile.image} height="150px" width="150px"
                                      className="rounded-circle wd-profile-picture bg-white ms-2" alt=""/>
                             </div>
                         </div>
                     </div>
 
                     <div className="wd-profile-basic">
-                        <span><h2 className="m-0">{profile.name.firstName} {profile.name.lastName}</h2>
+                        <span><h2 className="m-0">{user.name.firstName} {user.name.lastName}</h2>
                         <Link to="/student/edit-profile">
                             <button className="btn btn-outline-primary rounded-pill float-end m-2">Edit Profile
                             </button>
                         </Link></span>
-                        <div className="mb-3">{profile.headline}</div>
-                        <div className="mb-3"><b>LinkedIn:</b> <a href={profile.linkedInURL}>{profile.linkedInURL}</a>
-                        <div className="mb-3"><b>GitHub:</b> <a href={profile.githubURL}>{profile.githubURL}</a>
-                        <div className="mb-3"><b>Website:</b> <a href={profile.website}>{profile.website}</a>
+                        <div className="mb-3"><b>LinkedIn:</b> <a
+                            href={user.profile.linkedInURL}>{user.profile.linkedInURL}</a>
+                            <div className="mb-3"><b>GitHub:</b> <a
+                                href={user.profile.githubURL}>{user.profile.githubURL}</a>
+                                <div className="mb-3"><b>Website:</b> <a
+                                    href={user.profile.website}>{user.profile.website}</a>
+                                </div>
+                                <div className="mb-3"><b>Location:</b> {user.profile.location}</div>
+                                <div className="mb-3"><b>Contact info:</b>
+                                    <a href={user.email}> {user.email}</a>
+                                    <a href={user.profile.mobileNumber}> {user.profile.mobileNumber}</a>
+                                    {user.profile.address}
+                                </div>
+                            </div>
                         </div>
-                        <div className="mb-3"><b>Location:</b> {profile.location}</div>
-                        <div className="mb-3"><b>Contact info:</b>
-                            <a href={profile.email}> {profile.email}</a>
-                            <a href={profile.mobileNumber}> {profile.mobileNumber}</a>
-                            {profile.address}
+                        <div className="container bg-white border rounded-2 mt-4">
+                            <div className="mb-2 mt-2"><h4>About</h4></div>
+                            {user.profile.aboutMe}
                         </div>
-                        <div className="mb-3">{profile.summary}</div>
+
+                        <div className="container bg-white border rounded-2 mt-4">
+                            <div className="mb-2 mt-2"><h4>Education</h4></div>
+                            <h5>{user.profile.university}</h5><br/>
+                            {user.profile.educationLevel}<br/>
+                            {user.profile.major}<br/>
+                            {user.profile.department}<br/>
+                            {user.profile.graduationDate}<br/>
+                            {user.profile.GPA}
+                        </div>
+
+                        <div className="container bg-white border rounded-2 mt-4">
+                            <div className="mb-2 mt-2"><h4>Work Experience</h4></div>
+                            <h5>{user.profile.companyName}</h5><br/>
+                            {user.profile.position}<br/>
+                            {user.profile.workExperience} years
+                        </div>
+                        <div className="container bg-white border rounded-2 mt-4">
+                            <div className="mb-2 mt-2"><h4>Skills</h4></div>
+                            <ul>
+                            {/*    user.profile.skills.map({skill => {*/}
+                            {/*    return (*/}
+                            {/*        <li>{skill}</li>*/}
+                            {/*    )*/}
+                            {/*}})*/}
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                <div className="container bg-white border rounded-2 mt-4">
-                    <div className="mb-2 mt-2"><h4>About</h4></div>
-                    {profile.aboutMe}
-                </div>
-
-                <div className="container bg-white border rounded-2 mt-4">
-                    <div className="mb-2 mt-2"><h4>Education</h4></div>
-                    <h5>{profile.university}</h5><br/>
-                    {profile.educationLevel}<br/>
-                    {profile.major}<br/>
-                    {profile.department}<br/>
-                    {profile.graduationDate}<br/>
-                    {profile.GPA}
-                </div>
-
-                <div className="container bg-white border rounded-2 mt-4">
-                    <div className="mb-2 mt-2"><h4>Work Experience</h4></div>
-                    <h5>{profile.companyName}</h5><br/>
-                    {profile.position}<br/>
-                    {profile.workExperience} years
-                </div>
-                <div className="container bg-white border rounded-2 mt-4">
-                    <div className="mb-2 mt-2"><h4>Skills</h4></div>
-                    <ul>
-                        {profile.skills.map(skill => {
-                            return (
-                                <li>{skill}</li>
-                            )
-                        })}
-                    </ul>
-                </div>
-            </div>
-            </div>
             </div>
         )
     )
