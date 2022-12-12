d9e886db818ce1a68e6d83fedccc2503f103b32c
Gowtham Potnuru <gouthamcall@gmail.com>
2022-12-12 00:09:55 -0500

    updated

--- src/services/profile/profile-service.js
+++ src/services/profile/profile-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_BASE = process.env.SERVER_URL || "http://localhost:4000";
+const API_BASE = process.env.SERVER_URL;
 const PROFILE_API = `${API_BASE}/profile`;
 
 export const findProfileById = async (uid) => {
